aa5eccb7439cc432083acbcd42d63fdd6a5ac829
Sublele <benle126@126.com>
2016-12-04 14:51:23 +0800

    modify subdir.mk for QoSForMOHAPF

--- lte-sim-r5/Debug/src/flows/QoS/subdir.mk
+++ lte-sim-r5/Debug/src/flows/QoS/subdir.mk
@@ -7,18 +7,21 @@ CPP_SRCS += \
 ../src/flows/QoS/QoSForEXP.cpp \
 ../src/flows/QoS/QoSForFLS.cpp \
 ../src/flows/QoS/QoSForM_LWDF.cpp \
+../src/flows/QoS/QoSForMOHAPF.cpp \
 ../src/flows/QoS/QoSParameters.cpp 
 
 OBJS += \
 ./src/flows/QoS/QoSForEXP.o \
 ./src/flows/QoS/QoSForFLS.o \
 ./src/flows/QoS/QoSForM_LWDF.o \
+../src/flows/QoS/QoSForMOHAPF.o \
 ./src/flows/QoS/QoSParameters.o 
 
 CPP_DEPS += \
 ./src/flows/QoS/QoSForEXP.d \
 ./src/flows/QoS/QoSForFLS.d \
 ./src/flows/QoS/QoSForM_LWDF.d \
+../src/flows/QoS/QoSForMOHAPF.d \
 ./src/flows/QoS/QoSParameters.d 
 
 
